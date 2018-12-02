18bbc9ca222e3233458766013099e4a4d3ef93da
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-02 15:15:05 -0600

    Solve day 2 (part 2) problem

--- src/main/java/aoc2018/Day2.java
+++ src/main/java/aoc2018/Day2.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -50,9 +51,32 @@ public class Day2 {
         return countTwo * countThree;
     }
 
-    private static int part2() throws IOException {
-        // TODO
-        return 0;
+    private static String part2() throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(INPUT));
+        for (String line1 : lines) {
+            for (String line2 : lines) {
+                String diff = diff(line1, line2);
+                if (diff != null) {
+                    return diff;
+                }
+            }
+        }
+        return "";
+    }
+
+    private static String diff(String s1, String s2) {
+        int count = 0;
+        int index = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                count++;
+                index = i;
+            }
+        }
+        if (count != 1) {
+            return null;
+        }
+        return s1.substring(0, index) + s1.substring(index + 1);
     }
 
     public static void main(String[] args) throws Exception {
